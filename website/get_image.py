20df6e02aaa9971f57ef6f54b155d591f1b7af46
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2013-06-10 23:09:50 +0200

    Add legend to image

--- website/get_image.py
+++ website/get_image.py
@@ -33,7 +33,7 @@ show = utils.show
 PgConn    = utils.get_dbconn()
 PgCursor  = PgConn.cursor()
 
-def draw_polygon(rel_id, params, name, color, zorder=0):
+def draw_polygon(rel_id, params, name, color, zorder=0, label=""):
   if name != "" and rel_id != -1:
     sql = """select ST_AsText(ST_Union((SELECT geom from polygons
                                         where id = %s AND params = %s),
@@ -59,17 +59,31 @@ def draw_polygon(rel_id, params, name, color, zorder=0):
   geom = shapely.wkt.loads(wkt_geom)
   if hasattr(geom, "exterior"):
     a = numpy.asarray(geom.exterior)
-    pyplot.plot(a[:,0], a[:,1], color=color, zorder=zorder)
+    pyplot.plot(a[:,0], a[:,1], color=color, zorder=zorder, label=label)
   else:
+    label_shown = False
     for g in geom:
       a = numpy.asarray(g.exterior)
-      pyplot.plot(a[:,0], a[:,1], color=color, zorder=zorder)
+      if label_shown:
+        pyplot.plot(a[:,0], a[:,1], color=color, zorder=zorder)
+      else:
+        label_shown = True
+        pyplot.plot(a[:,0], a[:,1], color=color, zorder=zorder, label=label)
 
   return geom.bounds
 
 fig = pyplot.figure(1, dpi=100)
 
-(minx, miny, maxx, maxy) = draw_polygon(rel_id, params, name, "blue", zorder=10)
+if rel_id != -1 and name != "":
+  label = "union"
+elif rel_id == -1:
+  label = "user polygon"
+elif params != "0":
+  label = "simplified geometry"
+else:
+  label="base geometry"
+
+(minx, miny, maxx, maxy) = draw_polygon(rel_id, params, name, "blue", zorder=10, label=label)
 
 diff_x = maxx - minx
 diff_y = maxy - miny
@@ -79,11 +93,11 @@ diff_x_fixed = diff_x
 diff_y_fixed = diff_y / math.cos(math.radians((miny+maxy)/2))
 
 if diff_x_fixed > diff_y_fixed:
-  fig_height = int(diff_y_fixed / diff_x_fixed * 10)
-  fig_width = 10
-else:
-  fig_width = int(diff_x_fixed / diff_y_fixed * 10)
   fig_height = 10
+  fig_width = int(diff_x_fixed / diff_y_fixed * 10)
+else:
+  fig_width = 10
+  fig_height = int(diff_y_fixed / diff_x_fixed * 10)
 
 if fig_width > 200:
   fig_width = 200
@@ -96,12 +110,14 @@ ax.set_xlim(left=minx - 0.05*diff_x, right=maxx + 0.05*diff_x)
 ax.set_ylim(bottom=miny - 0.05*diff_y, top=maxy + 0.05*diff_y)
 ax.autoscale_view()
 
-if rel_id != -1 and (params != 0 or name != ""):
-  draw_polygon(rel_id, "0", "", "green", zorder=1)
+if rel_id != -1 and (params != "0" or name != ""):
+  draw_polygon(rel_id, "0", "", "green", zorder=1, label="base geometry")
 
-if name != "":
-  draw_polygon(-1, "", name, "orange", zorder=2)
+if name != "" and rel_id != -1:
+  draw_polygon(-1, "", name, "orange", zorder=2, label="user polygon")
 
+pyplot.legend(loc='upper center', ncol=5, frameon=True,
+              bbox_to_anchor=(0.5, 1), bbox_transform=pyplot.gcf().transFigure)
 fig.tight_layout()
 
 import cStringIO
