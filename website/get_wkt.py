fef47df620cd1f218465fe8fa0b637d283dcbd3c
Jocelyn Jaubert <jjaubert@openstreetmap.fr>
2014-12-17 19:46:38 +0100

    Send error 500 on error when getting wkt

--- website/get_wkt.py
+++ website/get_wkt.py
@@ -15,14 +15,21 @@ show = utils.show
 PgConn    = utils.get_dbconn()
 PgCursor  = PgConn.cursor()
 
-show(u"Content-Type: text/plain; charset=utf-8")
-print
-
 sql = """select ST_AsEWKT(geom)
          from polygons where id = %s AND params = %s"""
 PgCursor.execute(sql, (rel_id, params))
 
 results = PgCursor.fetchall()
+
+if len(results) == 0:
+  show(u"Status: 500")
+  show(u"Content-Type: text/plain; charset=utf-8")
+  print
+  show(u"Error: Polygons wasn't correctly generated")
+  sys.exit(0)
+
+show(u"Content-Type: text/plain; charset=utf-8")
+print
 
 for res in results:
     show(u"%s" % res[0])
