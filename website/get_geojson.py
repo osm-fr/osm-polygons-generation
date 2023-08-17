3f58cf26a8ea2d65383aaf3a2a568986b91f8819
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2023-08-17 20:36:45 +0200

    Make geojson dump more robust against errors

--- website/get_geojson.py
+++ website/get_geojson.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import cgi
+import json
 import os
 import sys
 
@@ -9,29 +10,52 @@ root = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(root)
 from tools import utils
 
-form      = cgi.FieldStorage()
-rel_id    = [int(i) for i in form.getvalue("id", "-1").split(",")]
-params    = str(form.getvalue("params", 0))
+form = cgi.FieldStorage()
 
-PgConn    = utils.get_dbconn()
-PgCursor  = PgConn.cursor()
+try:
+    rel_id = [int(i) for i in form.getvalue("id", "-1").split(",")]
+    params = str(form.getvalue("params", 0))
+    if params == "0":
+        (x, y, z) = (0, 0, 0)
+    else:
+        (x, y, z) = [float(i) for i in params.split("-")]
+        params = "%f-%f-%f" % (x, y, z)
+except:
+    print("Status: 400 Bad Request")
+    print("Content-Type: application/json; charset=utf-8")
+    print("")
+    print(json.dumps({"status": "ERROR", "message": "Parameters not supported"}))
+    sys.exit(0)
 
-print("Content-Type: application/geo+json; charset=utf-8")
-print("")
-
-if params == "0":
-    (x, y, z) = (0, 0, 0)
-else:
-    (x, y, z) = [float(i) for i in params.split("-")]
+PgConn   = utils.get_dbconn()
+PgCursor = PgConn.cursor()
 
 for id in rel_id:
-    utils.check_polygon(PgCursor, id, x, y, z, create=True)
+    try:
+        utils.check_polygon(PgCursor, id, x, y, z, create=True)
+    except (utils.NonExistingRelation,
+            utils.InvalidGeometry,
+            utils.InvalidSimplifiedGeometry) as e:
+        print("Status: 500 Internal Server Error")
+        print("Content-Type: application/json; charset=utf-8")
+        print("")
+        print(json.dumps({"status": "ERROR", "message": "Polygon couldn't be generated"}))
+        sys.exit(0)
 
 sql = """select ST_AsGeoJSON(ST_Union(geom))
          from polygons where id IN %s AND params = %s"""
 PgCursor.execute(sql, (tuple(rel_id), params))
 
 results = PgCursor.fetchall()
+
+if results == None:
+    print("Status: 500 Internal Server Error")
+    print("Content-Type: application/json; charset=utf-8")
+    print("")
+    print(json.dumps({"status": "ERROR", "message": "Polygon is empty"}))
+
+print("Content-Type: application/geo+json; charset=utf-8")
+print("")
 
 for res in results:
     print(res[0])
